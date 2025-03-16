5e517e3be11542f05ca2fa865d780ae48244182c
RennickLad <ianrennickspain@gmail.com>
2025-03-16 16:44:32 +0000

    added-writings

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :writings
     get "info", to: "pages#info"
     # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+10.times do |x|
+    Writing.create(title: "Title #{x + 1}", body: "Body #{x + 1} words go here IDK")
+end
